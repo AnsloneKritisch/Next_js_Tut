54d82760b90fb560376517bf591a1ae3cb1e0074
AnsloneKritisch <digveshoptimus@gmail.com>
2025-12-07 08:55:00 +0530

    new

---
+++ 01_Introduction/app/app/components/hello.tsx
@@ -0,0 +1,10 @@
+'use client' ;
+// This is line wiil make sure it is client component
+
+const hello = () => {
+  return (
+    <div>hello</div>
+  )
+}
+
+export default hello
---
+++ 01_Introduction/app/app/page.tsx
@@ -0,0 +1,6 @@
+const Home = () => {
+  return (
+    <div className="text-5xl underline">Welcome to Next.js! </div>
+  )
+}
+export default Home
